0f404896fce63d97df0f50b9d770a0e4dedd24b5
Mauro Castro <maurocastruli@gmail.com>
2026-07-13 16:21:08 -0300

    working

--- escenas/Game_over/game_over.gd
+++ escenas/Game_over/game_over.gd
@@ -2,17 +2,4 @@ extends Control
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://MainMenu.tscn")
-
-func _ready():
-	update_high_score_display()
-
-func update_high_score_display():
-	var high_scores = Global.load_high_scores()
-	var text = "🏆 HIGH SCORES 🏆\n"	
-	for i in range(high_scores.size()):
-		var entry = high_scores[i]
-		text += str(i + 1) + ". " + entry["name"] + " - " + str(entry["score"]) + " Pts (Nivel " + str(entry["level"]) + ")\n"    
-		%TOP10.text = text
-	print(text)
-	
+	get_tree().change_scene_to_file("res://escenas/main_menu/MainMenu.tscn")
