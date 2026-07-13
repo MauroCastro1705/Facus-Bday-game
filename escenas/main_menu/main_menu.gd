59ee19c4b516db08911ec3355e7b611e6cdfa858
Mauro Castro <maurocastruli@gmail.com>
2026-07-13 16:30:49 -0300

    a

--- escenas/main_menu/main_menu.gd
+++ escenas/main_menu/main_menu.gd
@@ -1,7 +1,11 @@
 extends Control
 
 @export var fade_duration: float = 0.5
+@onready var tutorial: Button = $tutorial
+@onready var tutorial_info: ColorRect = $tutorial/tutorial_info
 
+func _ready() -> void:
+	tutorial_info.hide()
 
 func _on_start_button_pressed():
 	Global.playerNAME = %name_input.text
@@ -17,3 +21,12 @@ func _on_creditos_pressed() -> void:
 	
 	# Cambiar a la escena del main menu
 	get_tree().change_scene_to_file("res://escenas/creditos/creditos.tscn")
+
+
+func _on_tutorial_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		tutorial.text = "X - cerrar"
+		tutorial_info.show()
+	else:
+		tutorial.text = "Tutorial"
+		tutorial_info.hide()
